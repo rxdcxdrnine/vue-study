3b79728adac60f11509ac3550a68d1c29aec4343
rxdcxdrnine <rkckr1@naver.com>
2021-07-12 17:29:26 +0900

    initial commit with test proj

---
+++ test/src/views/DataBindingList2.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>제품명</th>
+          <th>가격</th>
+          <th>카테고리</th>
+          <th>배송료</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(product, i) in productList">
+          <td>{{ product.product_name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.delivery_price }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import $api from "../mixins";
+
+export default {
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    async getList() {
+      this.productList = await $api(
+        "https://90f4c35d-5023-43a0-9ab0-06ef6499adb3.mock.pstmn.io/list",
+        "get"
+      );
+      console.log(this.productList);
+    },
+  },
+};
+</script>
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
---
+++ test/src/views/NestedComponents.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <PageTitle title="부모 컴포넌트로부터 자식 컴포넌트로 정적 데이터 잔달" />
+  </div>
+</template>
+<script>
+import PageTitle from "../components/PageTitle";
+export default {
+  components: { PageTitle },
+  data() {
+    return {
+      title: "동적 페이지 타이틀",
+    };
+  },
+};
+</script>
