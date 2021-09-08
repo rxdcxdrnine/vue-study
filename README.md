7096c128fd343c405c39fd9854dd8166151d6380
Kang Changgu <rkckr1@naver.com>
2021-09-08 15:41:53 +0900

    delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-study
-vue study with vue.js 프로젝트 투입 일주일 전
--- test/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import mixins from "./mixins";
-
-const app = createApp(App);
-app.use(router);
-app.mixin(mixins);
-app.mount("#app");
